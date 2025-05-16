dc0d7115673d7a403164470beb46dddacbe02bf3
Hyeonu Yu <limeorange1102@gmail.com>
2025-05-16 13:35:01 +0900

    S

--- preprocessing.py
+++ preprocessing.py
@@ -160,7 +160,7 @@ json_folder = "/content/drive/MyDrive/lip_audio_multimodal/input_texts"
 video_folder = "/content/drive/MyDrive/lip_audio_multimodal/input_videos"
 
 # 출력 경로
-landmark_npy_dir = "C:\Users\vips1\OneDrive\Desktop\과제\종설\processed_dataset\landmark_npy"
+landmark_npy_dir = "/content/drive/MyDrive/lip_audio_multimodal/processed_dataset/landmark_npy"
 
 # landmark 좌표 추출 및 저장 실행
 extract_lip_landmarks_all(json_folder, video_folder, landmark_npy_dir)
